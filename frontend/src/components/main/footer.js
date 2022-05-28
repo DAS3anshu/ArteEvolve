40ee9b030d261e1195a5d7d90942d270e3d42953
DAS3anshu <anshikaagrahari607@gmail.com>
2022-05-28 13:40:15 +0530

    ..

--- frontend/src/components/main/footer.js
+++ frontend/src/components/main/footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-light text-center text-white">
-      <div className="container p-4 pb-0">
-        <section className="mb-4">
+    <footer className="bg-light text-center text-white bg-dark mt-2">
+      <div className="container p-4 pb-0" >
+        <section className="mb-4" >
           <a
             className="btn btn-primary btn-floating m-1"
             style={{ backgroundColor: "#3b5998" }}
@@ -52,16 +52,93 @@ const Footer = () => {
 
           <a
             className="btn btn-primary btn-floating m-1"
-            style={{ backgroundColor: "#333333" }}
+            style={{ backgroundColor: "black" }}
             href="#!"
             role="button"
           >
             <i className="fab fa-github"></i>
           </a>
         </section>
+        <section class="">
+    <div class="container text-center text-md-start mt-5">
+      
+      <div class="row mt-3">
+        
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Art Evolve
+          </h6>
+          <p>
+            Art Evolve is a website where art lover can experience the exhibition
+            bt VR device which can make you feel so realistic and make you feel 
+            like you are present in the art gallery.
+          </p>
+        </div>
+        
+        {/* <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Angular</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">React</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vue</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Laravel</a>
+          </p>
+        </div> */}
+        
+
+        
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Pricing</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Settings</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Orders</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
+        
+
+        
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contact
+          </h6>
+          <p><i class="fas fa-home me-3"></i> Babu Banarasi Das University, Lucknow</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            simranamb3@bbdu.ac.in
+          </p>
+          <p><i class="fas fa-phone me-3"></i> +91 9616628302</p>
+          <p><i class="fas fa-print me-3"></i> +91 7394028848</p>
+        </div>
+        
+      </div>
+     
+    </div>
+  </section>
       </div>
 
-      <div className="text-center p-3" style={{ backgroundColor: "#44004982" }}>
+      <div className="text-center p-3" style={{ backgroundColor: "black" }}>
         © {new Date().getFullYear()} Copyright:
         <a className="text-white" href="https://digipodium.com/">
           Digipodium
--- frontend/src/components/main/home.js
+++ frontend/src/components/main/home.js
@@ -28,14 +28,7 @@ const Home = () => {
             <div className="col-md-4">
               <div className="card">
                 <div className="card-body">
-                  <img
-                    style={{
-                      height: "10rem",
-                      display: "block",
-                      margin: "auto",
-                    }}
-                    src="https://vrallart.com/icons/stakeholders/SPACE.svg"
-                  />
+                  <img  style={{ height: "10rem",  display: "block",margin: "auto",  }}src="https://vrallart.com/icons/stakeholders/SPACE.svg" alt=""/>
                   <h1><b>Expand Space</b></h1>
                   <hr />
                   <p>Broaden your gallery space and showcase
@@ -48,14 +41,7 @@ const Home = () => {
             <div className="col-md-4">
               <div className="card">
                 <div className="card-body">
-                  <img 
-                     style={{
-                      height: "10rem",
-                      display: "block",
-                      margin: "auto",
-                     }}
-                     src="https://vrallart.com/icons/stakeholders/TIME.svg"
-                     />
+                  <img style={{ height: "10rem", display: "block", margin: "auto",}}src="https://vrallart.com/icons/stakeholders/TIME.svg" alt="" />
                   <h1><b>Extend Time</b></h1>
                   <hr />
                   <p>Prolong the lifespan of your 
@@ -68,81 +54,69 @@ const Home = () => {
             <div className="col-md-4">
               <div className="card">
                 <div className="card-body">
-                <img 
-                     style={{
-                      height: "10rem",
-                      display: "block",
-                      margin: "auto",
-                     }}
-                     src="https://vrallart.com/icons/stakeholders/AUDIENCE.svg"
-                     />
+                <img style={{height: "10rem",display: "block",margin: "auto",}} src="https://vrallart.com/icons/stakeholders/AUDIENCE.svg" alt=""/>
                   <h1><b>Incrase Audience</b></h1>
                   <hr />
                   <p>Reach global visitors and collectors 
-                    through the VR-ALL-ART platform.
+                    through the ART-EVOLVE platform.
                   </p>
                 </div>
               </div>
             </div>
           </div>
-          <button
-            className="btn btn-outline-transparent my-5"
-            onClick={(e) => navigate("/main/browse")}
-          >
-            SEE Exhibition Space
-          </button>
+          
         </div>
 
       </section>
 
 
       
+    <section>
+      <div className="container bg-light p-5  mt-5">
+        <div class="row">
+          <div class="col-sm-6 bg-transparent w-50" style={{ flexBasis: "50%",textAlign: "left", padding: "0 15px"}}>    
+            <h2 class="h1">Virtual Reality</h2><br></br>
+            <p class="p"><b>Virtual Reality exhibitions</b> showcase artworks in virtual spaces. 
+            They are created by taking high quality scans or photographs of physical art pieces which are then curated in a virtual space using the VR-All-Art web editor.
+            With the ability to edit your exhibitions, 
+            you can arrange the position of artworks and change the lighting and color of the walls as needed..</p>   
+          </div>
+          <div class="col-sm-6 bg-transparent w-50">
+            <iframe width="550" height="300" src="https://www.youtube.com/embed/3ewO-gy1VKo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div className="container p-3 ">
+        <div class="row my-4">
+          <div class="col-sm-6 bg-transparent w-50">
+            <iframe width="550" height="300" src="https://www.youtube.com/embed/TXdzJnT71zg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <div class="col-sm-6 bg-transparent w-50" style={{ flexBasis: "50%",textAlign: " left", padding: "0 9.5px"}}>    
+            <h2 class="h1">Augmented Reality</h2><br></br>
+            <p class="p"> The<b> Augmented Reality exhibition</b>      
+              is a mobile-accessible virtual space that imitates a real-life exhibition.
+               Using Augmented Reality technology, you can visualize virtual exhibitions and objects in 
+               your real space through the window of your mobile phone or tablet. Simply place scanned art
+                pieces into your surrounding environment. Use the VR-All-Art AR app to showcase your artworks to potential buyers
+                 wherever you go.</p>   
+          </div>
+        </div>
+      </div>
+    </section>
+          
       <section>
-      <div className="container bg-light p-5 ">
-      <div class="row">
-  <div class="col-sm-6 bg-transparent w-50">
-    
-      
-        <h2 class="h1">Virtual Reality</h2>
-        <p class="p">Virtual Reality exhibitions showcase artworks in virtual spaces. 
-        They are created by taking high quality scans or photographs of physical art pieces which are then curated in a virtual space using the VR-All-Art web editor.
-         With the ability to edit your exhibitions, 
-        you can arrange the position of artworks and change the lighting and color of the walls as needed..</p>
-        
-      
-    
-  </div>
-  <div class="col-sm-6 bg-transparent w">
-    
-      
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        
-      
-    
-  </div>
-</div>
-</div>
-      </section>
-      
-      <section>
-        <div className="container text-muted text-center">
-          <h2 >MOBILITY</h2>
-          <h3 className="display-6">Take your exhibition with you wherever you go</h3>
-          <h4>Transform your exhibition space into a mobile gallery and take it with you when you travel</h4>
-          <div className="row text-center">
+        <div className="container  text-muted text-center bg-light mt-5 ">
+          <h2 className="p-3"><u><b>MOBILITY</b></u></h2>
+          <h4 className="display-6">Take your exhibition with you wherever you go</h4>
+          <h5>Transform your exhibition space into a mobile gallery and take it with you when you travel</h5>
+          <div className="row text-center p-3">
             <div className="col-md-4">
               <div className="card">
                 <div className="card-body">
-                  <img
-                    style={{
-                      height: "10rem",
-                      display: "block",
-                      margin: "auto",
-                    }}
-                    src="https://vrallart.com/icons/stakeholders/MEETINGS.svg"
-                  />
-                  <h4><b>Bring To Meeting</b></h4>
+                  <img style={{ height: "10rem", display: "block", margin: "auto", }} src="https://vrallart.com/icons/stakeholders/MEETINGS.svg" alt=""/>
+                  <h3><b>Bring To Meeting</b></h3>
                   <hr />
                   <p>Show collectors your past and current 
                     exhibitions in your virtual gallery sapce.
@@ -153,15 +127,9 @@ const Home = () => {
             <div className="col-md-4">
               <div className="card">
                 <div className="card-body">
-                  <img 
-                     style={{
-                      height: "10rem",
-                      display: "block",
-                      margin: "auto",
-                     }}
-                     src="https://vrallart.com/icons/stakeholders/ARTFAIRS.svg"
-                     />
-                  <h4><b>Take To Art Fair</b></h4>
+                  <img style={{height: "10rem",display: "block",margin: "auto", }} src="https://vrallart.com/icons/stakeholders/ARTFAIRS.svg" alt=""/>
+                   
+                  <h3><b>Take To Art Fair</b></h3>
                   <hr />
                   <p>Present an unlimited number of artworks and 
                     exhibitions regardless of your booth size.
@@ -172,17 +140,11 @@ const Home = () => {
             <div className="col-md-4">
               <div className="card">
                 <div className="card-body">
-                <img 
-                     style={{
-                      height: "10rem",
-                      display: "block",
-                      margin: "auto",
-                     }}
-                     src="https://vrallart.com/icons/stakeholders/ONLINE.svg"
-                     />
-                  <h4><b>Share Online</b></h4>
-                  <hr />
-                  <p>share a link to your exhibition with anyone who has VR equipment.
+                <img style={{ height: "8.5rem",display: "block",margin: "auto",}}src="https://vrallart.com/icons/stakeholders/ONLINE.svg" alt=""/>
+                   
+                  <h3><b>Share Online</b></h3>
+                  <hr/>
+                  <p>Share a link to your exhibition with anyone who has VR equipment.
                     Embed exhibition on your websites.
                   </p>
                 </div>
@@ -193,7 +155,10 @@ const Home = () => {
         </div>
 
       </section>
-    </div>
+    
+  </div>
+
+  
   );
 };
 
